5c057d1560f80c5f1f46e1581677b86114f2ffe9
Lucas <lucasoliveirars22@gmail.com>
2025-02-07 09:16:06 -0300

    :sparkles: add initial pages

--- src/app/pet/[id]/page.tsx
+++ src/app/pet/[id]/page.tsx
@@ -1,11 +1,9 @@
-export default function Login() {
+export default function PetPage() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
       <section className="flex flex-col items-center p-8 bg-white shadow-lg rounded-lg">
         <h1 className="text-4xl font-bold mb-4">EmotiPet</h1>
-        <p className="text-lg text-gray-600 mb-6">
-          Bem-vindo à Página Inicial (login)
-        </p>
+        <p className="text-lg text-gray-600 mb-6">Bem-vindo à Página do Pet</p>
         <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
           Clique aqui
         </button>
