49355f9082a9a1e271eb2d30466c803225049543
Davy Dantas <davy.dantas@escolar.ifrn.edu.br>
2025-02-11 12:43:42 -0300

    efeito de typing no modulo da imagem principal

--- src/components/MainImageIndex/index.tsx
+++ src/components/MainImageIndex/index.tsx
@@ -1,27 +1,31 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "@/components/MainImageIndex/style.module.css";
+import Typewriter from 'typewriter-effect'; 
+
 
 export function MainImageIndex() {
-  const [Text, setText] = useState([
+  const [Text] = useState([
     "Qual é a comida preferida do meu pet ?",
     "Qual é a brincadeira preferida do meu pet ?",
     "Como devo cuidar do meu pet ?",
     "Como saber se meu pet está feliz ?",
-  ])
+  ]);
 
-  const randomText = () => {
+  const [randomText, setRandomText] = useState("");
+
+  useEffect(() => {
     const random = Math.floor(Math.random() * Text.length);
-    return Text[random];
-  }
+    setRandomText(Text[random]);
+  }, [Text]);
 
   return (
     <div className="relative inline-block">
       <div className="relative mt-32 ml-16 flex items-end justify-center">
 
-      <div className="absolute z-20 top-40 left-96 w-max">
+      <div className="absolute z-20 top-40 left-96 w-[52px] h-[52px]">
           <Image
             src="/assets/svg/icone-IA.svg"
             alt="EmotiPet"
@@ -29,8 +33,13 @@ export function MainImageIndex() {
             height={52}
             className="absolute -top-14 -left-10"
           />
-        <div className="bg-whiteOrange py-1 px-3 rounded-full">
-          <p className="text-xl text-gray">{randomText()}</p>
+        <div className="bg-whiteOrange py-1 px-3 rounded-full text-lg text-gray w-max">
+            <Typewriter onInit={(typewriter) => {
+            typewriter.typeString(randomText)
+            .pauseFor(1000)
+            .start();
+            }} options={{ delay: 80 }}>
+            </Typewriter>
         </div>
       </div>
 
