91200b815373218b532640cf457d56982aa11312
Lucas <lucasoliveirars22@gmail.com>
2025-03-05 18:46:28 -0300

    :sparkles: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const publicRoutes = ["/", "/register"];
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  console.log("Token: ", token);
+
+  if (token) {
+    return NextResponse.next();
+  }
+
+  if (!token && !publicRoutes.includes(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/home", "/pet/:slug*", "/user/:userId*", "/", "/register"],
+};
