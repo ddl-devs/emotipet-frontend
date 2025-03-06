56c02d2c21ac1234c0fe2c9684be28e38bafa151
Lucas <lucasoliveirars22@gmail.com>
2025-03-06 19:52:00 -0300

    ✨  feat: E2E test user profile editing (#27)
    * :construction: e2e test update user profile
    
    * :sparkles: e2e test edit profile finished

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,8 @@
-describe("template spec", () => {
+describe("Login spec", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  it("login successfully", () => {
+  it("login com sucesso", () => {
     cy.get('input[name="username"]').type(Cypress.env("LOGIN_USERNAME"));
     cy.get('input[name="password"]').type(Cypress.env("LOGIN_PASSWORD"));
 
@@ -13,7 +13,7 @@ describe("template spec", () => {
     cy.contains("Bem-vindo à Página de Login").should("not.exist");
   });
 
-  it("login fail", () => {
+  it("login falhando", () => {
     cy.get('input[name="username"]').type(Cypress.env("LOGIN_USERNAME"));
     cy.get('input[name="password"]').type(Cypress.env("LOGIN_PASSWORD_FAIL"));
 
