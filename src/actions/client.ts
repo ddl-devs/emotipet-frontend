5e0318127206520729e6949ef4dbf17f51c83897
DamiaoFilho <damiaofilho0123@gmail.com>
2025-03-06 23:24:12 -0300

    list analysis

--- src/actions/client.ts
+++ src/actions/client.ts
@@ -1,7 +1,7 @@
 "use server";
 import { cookies } from "next/headers";
 
-const apiUrl = "https://backend.damdevops.com.br";
+const apiUrl = "http://localhost:8080";
 
 const getToken = async () => {
   const cookieStore = await cookies();
@@ -31,7 +31,7 @@ const apiClient = async (
       ...options.headers,
     },
   });
-
+  console.log(response);
   if (response.status === 401) {
     const wwwAuthenticate = response.headers.get("WWW-Authenticate");
     if (
---
+++ src/actions/getPetAnalysis.ts
@@ -0,0 +1,28 @@
+import apiClient from "./client";
+
+interface GetPetAnalysisParams {
+    page: number;
+    id: number;
+    startDate?: string;
+    endDate?: string;
+    type?: string;
+    result?: string;
+    status?: string;
+}
+
+export const getPetAnalysis = async ({ id, startDate, endDate, type, result, page, status }: GetPetAnalysisParams) => {
+    const queryParams = new URLSearchParams();
+
+    queryParams.append("page", String(page));
+    if (startDate) queryParams.append("startDate", startDate);
+    if (endDate) queryParams.append("endDate", endDate);
+    if (type) queryParams.append("type", type);
+    if (result) queryParams.append("result", result);
+    if (status) queryParams.append("status", status);
+
+    const url = `/pet-analysis/pet/${id}?${queryParams.toString()}`;
+    const response = await apiClient(url, {
+        method: "GET",
+    });
+    return response;
+};
