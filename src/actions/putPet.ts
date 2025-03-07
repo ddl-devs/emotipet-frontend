64aae5d69e88c06b01c6f7a13f13ca0ffbe1c252
Lucas <lucasoliveirars22@gmail.com>
2025-03-07 00:45:54 -0300

    :construction: edit profile pet

---
+++ src/actions/putPet.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { cookies } from "next/headers";
+import apiClient from "./client";
+
+const getToken = async () => {
+  const cookieStore = await cookies();
+  return cookieStore.get("token")?.value || null;
+};
+
+export async function putPet(idPet: number, formData: FormData) {
+  const token = await getToken();
+
+  if (!token) {
+    console.error("No token found");
+    return;
+  }
+
+  try {
+    console.log("formData", formData);
+    const response = await apiClient(`/pets/${idPet}`, {
+      method: "PUT",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response;
+    return data;
+  } catch (error) {
+    console.error("Request failed:", error);
+    throw error;
+  }
+}
--- src/components/ProfilePet/index.tsx
+++ src/components/ProfilePet/index.tsx
@@ -1,87 +1,215 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProfileInput from "@/components/ProfileInput";
 import EditButton from "@/components/EditButton";
 import DeleteButton from "@/components/DeleteButton";
-import style from "./style.module.css"
+import style from "./style.module.css";
 
+import { useParams } from "next/navigation";
+import { getPet } from "@/actions/getPet";
+import { putPet } from "@/actions/putPet";
+import { Pet } from "@/types/pets";
 
-export function ProfilePet(){
-    const [profileImage, setProfileImage] = useState("/assets/images/bolota.png");
+export function ProfilePet() {
+  const [profileImage, setProfileImage] = useState("/assets/images/bolota.png");
+  const [editable, setEditable] = useState(false);
+  const [pet, setPet] = useState<Pet | null>(null);
 
-    const [editable, setEditable] = useState(false);
+  const params = useParams();
+  const { id } = params;
 
-    const handleEditTrue = () => {
-        setEditable(true);
-    }
+  const [formData, setFormData] = useState({
+    name: "",
+    breed: "",
+    birthdate: "",
+    weight: "",
+    gender: "",
+    height: "",
+    profileImage: null as File | null,
+  });
 
-    const handleEditFalse = () => {
-        setEditable(false);
-    }
-
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-            const file = event.target.files[0];
-            const imageUrl = URL.createObjectURL(file);
-            setProfileImage(imageUrl);
-        }
+  useEffect(() => {
+    const fetchPet = async () => {
+      if (typeof id === "string") {
+        const petData = await getPet(id);
+        setPet(petData);
+        setFormData({
+          name: petData.name,
+          breed: petData.breed,
+          birthdate: formatDate(petData.birthdate),
+          weight: petData.weight,
+          gender: petData.gender,
+          height: petData.height,
+          profileImage: null,
+        });
+      }
     };
+    fetchPet();
+  }, [id]);
 
-    return (
-        <div className="flex relative flex-row">
-        
-        <div className={`relative flex flex-col bg-white shadow-lg w-[640px] h-auto justify-center items-center rounded-3xl ${editable ? style.profile : ''}`}>
-                
-                {editable &&
-                    <div className={`${style.button} opacity-0 ${editable ? 'transition-opacity opacity-100 duration-500' : 'transition-opacity opacity-0'} bg-green absolute bottom-3 right-3 rounded-full flex items-center justify-center animate-fade-in`}>
-                        <button onClick={handleEditFalse} className="text-lg text-white px-3 font-medium py-1">Salvar</button>
-                    </div>
-                }
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
 
-                <div className="items-center justify-center flex flex-col relative -top-[70]">
-                    <Image
-                        src={profileImage}
-                        alt="Bolota"
-                        width={140}
-                        height={140}
-                        className="rounded-full w-[140px] h-[140px]"
-                    />
-                    {editable && (
-                    <div className="mt-2">
-                        <label className="cursor-pointer text-blackGray font-semibold" htmlFor="profileImage">Trocar foto de perfil</label>
-                        <input
-                            id="profileImage"
-                            type="file"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                            className="mt-2 hidden"
-                            />
-                    </div>
-                    )}
-                    <div className=" flex flex-row gap-2">
-                        <ProfileInput editable={editable} classNm="font-bold text-2xl" input="Bolota" placeholder="Insira um nome" wid="250px" color="orange" id="name"/>
-                    </div>
-                </div>
+  const handleEditTrue = () => {
+    setEditable(true);
+  };
 
-                <div className="flex h-auto flex-wrap gap-5 -mt-10 mb-[50px] max-w-[80%] items-start justify-center">
-                    <div className="gap-5 flex items-start flex-col">
-                        <ProfileInput editable={editable} id="raca" input="Pastor Alemão" label="Raça:" placeholder="Digite uma raça" wid="220px"></ProfileInput>
-                        <ProfileInput editable={editable} type="date" id="data" input="24/02/2024" label="Data de Nascimento:" placeholder="Digite a data" wid="220px"></ProfileInput>
-                    </div>
-                    <div className="flex flex-wrap gap-5 items-start justify-start">
-                        <ProfileInput editable={editable} id="peso" input="30kg" label="Peso:" placeholder="Digite o peso" wid="70px"></ProfileInput>
-                        <ProfileInput editable={editable} id="sexo" input="Fêmea" label="Sexo:" placeholder="Digite o sexo" wid="80px"></ProfileInput>
-                        <ProfileInput editable={editable} id="altura" input="60cm" label="Altura:" placeholder="Digite a altura" wid="70px"></ProfileInput>
-                    </div>
-                </div>
+  const handleEditFalse = async () => {
+    setEditable(false);
+    const form = new FormData();
+    form.append("name", formData.name);
+    form.append("breed", formData.breed);
+    form.append("birthdate", formData.birthdate);
+    form.append("weight", formData.weight);
+    form.append("gender", formData.gender);
+    form.append("height", formData.height);
+    if (formData.profileImage) {
+      form.append("photoUrl", formData.profileImage);
+    }
+    if (pet?.id) {
+      await putPet(pet.id, form);
+      window.location.reload();
+    }
+  };
 
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const imageUrl = URL.createObjectURL(file);
+      setProfileImage(imageUrl);
+      setFormData({ ...formData, profileImage: file });
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  return (
+    <div className="flex relative flex-row">
+      <div
+        className={`relative flex flex-col bg-white shadow-lg w-[640px] h-auto justify-center items-center rounded-3xl ${
+          editable ? style.profile : ""
+        }`}
+      >
+        {editable && (
+          <div
+            className={`${style.button} opacity-0 ${
+              editable
+                ? "transition-opacity opacity-100 duration-500"
+                : "transition-opacity opacity-0"
+            } bg-green absolute bottom-3 right-3 rounded-full flex items-center justify-center animate-fade-in`}
+          >
+            <button
+              onClick={handleEditFalse}
+              className="text-lg text-white px-3 font-medium py-1"
+            >
+              Salvar
+            </button>
+          </div>
+        )}
+
+        <div className="items-center justify-center flex flex-col relative -top-[70]">
+          <Image
+            src={pet?.photoUrl ?? profileImage}
+            alt="Bolota"
+            width={140}
+            height={140}
+            className="rounded-full w-[140px] h-[140px]"
+          />
+          {editable && (
+            <div className="mt-2">
+              <label
+                className="cursor-pointer text-blackGray font-semibold"
+                htmlFor="profileImage"
+              >
+                Trocar foto de perfil
+              </label>
+              <input
+                id="profileImage"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="mt-2 hidden"
+              />
             </div>
-            <div className="absolute left-[calc(50%+320px)] flex flex-col ml-3 mt-1 gap-4">
-                <EditButton click={handleEditTrue}/>
-                <DeleteButton />
-            </div>
+          )}
+          <div className=" flex flex-row gap-2">
+            <ProfileInput
+              editable={editable}
+              classNm="font-bold text-2xl"
+              input={formData.name}
+              placeholder="Insira um nome"
+              wid="250px"
+              color="orange"
+              id="name"
+              onchange={handleInputChange}
+            />
+          </div>
         </div>
-  )
+
+        <div className="flex h-auto flex-wrap gap-5 -mt-10 mb-[50px] max-w-[80%] items-start justify-center">
+          <div className="gap-5 flex items-start flex-col">
+            <ProfileInput
+              editable={editable}
+              id="breed"
+              input={formData.breed}
+              label="Raça:"
+              placeholder="Digite uma raça"
+              wid="220px"
+              onchange={handleInputChange}
+            />
+            <ProfileInput
+              editable={editable}
+              type="date"
+              id="birthdate"
+              input={formData.birthdate}
+              label="Data de Nascimento:"
+              placeholder="Digite a data"
+              wid="220px"
+              onchange={handleInputChange}
+            />
+          </div>
+          <div className="flex flex-wrap gap-5 items-start justify-start">
+            <ProfileInput
+              editable={editable}
+              id="weight"
+              input={formData.weight}
+              label="Peso:"
+              placeholder="Digite o peso"
+              wid="70px"
+              onchange={handleInputChange}
+            />
+            <ProfileInput
+              editable={editable}
+              id="gender"
+              input={formData.gender}
+              label="Sexo:"
+              placeholder="Digite o sexo"
+              wid="80px"
+              onchange={handleInputChange}
+            />
+            <ProfileInput
+              editable={editable}
+              id="height"
+              input={formData.height}
+              label="Altura:"
+              placeholder="Digite a altura"
+              wid="70px"
+              onchange={handleInputChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="absolute left-[calc(50%+320px)] flex flex-col ml-3 mt-1 gap-4">
+        <EditButton click={handleEditTrue} />
+        <DeleteButton />
+      </div>
+    </div>
+  );
 }
